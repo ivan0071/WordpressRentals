667f38b462da78206905df3ad77610b7b850da65
IvanStojanov <ivan.s@scopicsoftware.com>
2019-10-23 13:23:52 +0200

    improve home categories design

--- wp-content/plugins/essential-real-estate/public/templates/shortcodes/home-categories/home-categories.php
+++ wp-content/plugins/essential-real-estate/public/templates/shortcodes/home-categories/home-categories.php
@@ -16,37 +16,48 @@ wp_enqueue_script(ERE_PLUGIN_PREFIX . 'home-categories', ERE_PLUGIN_URL . 'publi
 
 <div class="ere-home-categories-wrap">
 	<div class="row">
-		<div class="purple-box col-sm-6">
-			COMMERCIAL REAL ESTATE
-			<br><br>
-			CHECK OUT YOUR VAST SELECTION<br>
-			OF LISTINGS FOR COMMERCIAL<br>
-			PROPERTES FOR RENT AND SALES
-			<hr>
-			<?php //to do: handle links differently ?>
-			<span>
-				<a href='<?php echo get_permalink(4025)."?group=1&resid-type-search=1&resid-furnished-type-search=1&commer-offices-search=1&commer_offices=office;serviced-office" ?>'>OFFICE SPACE</a>
-			</span> * <span>
-				<a href='<?php echo get_permalink(4025)."?group=1&commer-industrial-search=1&commer_industrial=distribution-warehouse;warehouse" ?>'>WAREHOUSING</a>
-			</span> * <span>
-				<a href='<?php echo get_permalink(4025)."?group=1&commer-industrial-search=1&commer_industrial=factory;heavy-industrial;industrial-park;light-industrial" ?>'>PRODUCTION FACILITIES</a>
-			</span>
+		<div class="col-sm-6">
+			<div class="purple-box">
+				<div class="purple-box-header">
+					<span class="purple-box-title">COMMERCIAL REAL ESTATE</span>
+					<br><br>
+					CHECK OUT YOUR VAST SELECTION<br>
+					OF LISTINGS FOR COMMERCIAL<br>
+					PROPERTES FOR RENT AND SALES
+				</div>
+				<div class="purple-box-foother">
+					<?php //to do: handle links differently ?>
+					<span>
+						<a href='<?php echo get_permalink(4025)."?group=1&resid-type-search=1&resid-furnished-type-search=1&commer-offices-search=1&commer_offices=office;serviced-office" ?>'>OFFICE SPACE</a>
+					</span> * <span>
+						<a href='<?php echo get_permalink(4025)."?group=1&commer-industrial-search=1&commer_industrial=distribution-warehouse;warehouse" ?>'>WAREHOUSING</a>
+					</span> * <span>
+						<a href='<?php echo get_permalink(4025)."?group=1&commer-industrial-search=1&commer_industrial=factory;heavy-industrial;industrial-park;light-industrial" ?>'>PRODUCTION FACILITIES</a>
+					</span>
+				</div>
+			</div>
 		</div>
-		<div class="purple-box col-sm-6">
-			RESIDENTIAL REAL ESTATE
-			<br><br>
-			WEHTHER YOU ARE LOOKING TO RENT OR<br>
-			BUY YOUR NEXT HOME, SUMMER HOUSE OR<br>
-			TEMPORARY RESIDENCE, WE HAVE A LARGE<br>
-			SELECTION TO CHOOSE FROM
-			<hr>
-			<span>
-				<a href='<?php echo get_permalink(4025)."?group=0&resid-type-search=1&resid_type=detached-house;semi-detached-house;terraced-house" ?>'>HOUSES</a>
-			</span> * <span>
-				<a href='<?php echo get_permalink(4025)."?group=0&resid-type-search=1&resid_type=flat" ?>'>APARTMENTS</a>
-			</span> * <span>
-				<a href='<?php echo get_permalink(4025)."?group=0&resid-type-search=1&resid_type=bungalow" ?>'>VILLAS</a>
-			</span>
+
+		<div class="col-sm-6">
+			<div class="purple-box">
+				<div class="purple-box-header">
+					<span class="purple-box-title">RESIDENTIAL REAL ESTATE</span>
+					<br><br>
+					WEHTHER YOU ARE LOOKING TO RENT OR<br>
+					BUY YOUR NEXT HOME, SUMMER HOUSE OR<br>
+					TEMPORARY RESIDENCE, WE HAVE A LARGE<br>
+					SELECTION TO CHOOSE FROM
+				</div>
+				<div class="purple-box-foother">
+					<span>
+						<a href='<?php echo get_permalink(4025)."?group=0&resid-type-search=1&resid_type=detached-house;semi-detached-house;terraced-house" ?>'>HOUSES</a>
+					</span> * <span>
+						<a href='<?php echo get_permalink(4025)."?group=0&resid-type-search=1&resid_type=flat" ?>'>APARTMENTS</a>
+					</span> * <span>
+						<a href='<?php echo get_permalink(4025)."?group=0&resid-type-search=1&resid_type=bungalow" ?>'>VILLAS</a>
+					</span>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
